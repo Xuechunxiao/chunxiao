681ddd21a370977b85661a0bc74b6b87b569e031
春晓 <44227438052qq.com>
2017-12-19 14:51:44 +0800

    修改两次后的代码

--- app/src/main/java/com/example/gitfengzhi/MainActivity.java
+++ app/src/main/java/com/example/gitfengzhi/MainActivity.java
@@ -4,15 +4,27 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private ListView lv;
+    private List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        initDate();
+    }
+
+    private void initDate() {
+        list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add("git分支");
+        }
     }
 
     private void initView() {
