1a2575f36e3e57148ad51c9cbcf2f3cbd026f671
春晓 <44227438052qq.com>
2017-12-19 14:49:26 +0800

    修改一次后的代码

--- app/src/main/java/com/example/gitfengzhi/MainActivity.java
+++ app/src/main/java/com/example/gitfengzhi/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.gitfengzhi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        lv = (ListView) findViewById(R.id.lv);
     }
 }
